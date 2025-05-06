5b53148cacfa5bc79b8f1885f5d88b21a1d3d292
Rukhsana Wasiak <ruksclone@hotmail.com>
2025-05-06 16:50:28 +0100

    added middlewares

--- MyLeaveSsytem/src/types/IAuthenticatedJWTRequest.ts
+++ MyLeaveSsytem/src/types/IAuthenticatedJWTRequest.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { Role } from "../entity/Role";
+import { Request } from "express";
 
-export interface IAuthenticatedJWTRequest extends express.Request {
+export interface IAuthenticatedJWTRequest extends Request {
+    headers: any;
     signedInUser?: {
-        email?: string;
-        role?: Role;
+        email: string;
+        role: string;
     };
+    ip: string; 
 }
